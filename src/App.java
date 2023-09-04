9df63b7ba6731924105bd058792eece3bd05608a
Johan Erik Kateby <johanerik.kateby@ntig.se>
2023-09-04 08:52:40 +0200

    hämtar in födelsedatum som heltal

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Ange ditt födelsedatum");
+        Scanner tangenbord = new Scanner(System.in);
+
+        int födelse_datum = tangenbord.nextInt();
     }
 }
